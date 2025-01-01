8e79580e185da1184b9ddc8d2d5c1459e7c8d717
racelo <kraef@protonmail.com>
2025-01-01 10:19:25 +0800

    Create an individual function for each operator

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+"Use strict";
+
+// Step 1: Create a function for each operators: add, subtract, multiply, divide
+
+//+ Add
+function add(a, b) {
+  return a + b;
+}
+
+//+ Subtract
+function subtract(a, b) {
+  return b - a;
+}
+
+//+ Multiply
+function multiply(a, b) {
+  return a * b;
+}
+
+//+ Divide
+function divide(a, b) {
+  return b / a;
+}
