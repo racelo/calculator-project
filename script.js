4e2931b31838168d8f65db709bc073c6f94bf8d1
racelo <kraef@protonmail.com>
2026-02-08 14:28:01 +0800

    Add a clear button and fix the euqal sign button so as to allow the users to use the result of the previous operation for the next set of calculation

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// const mathOp = {
-//     add: function(n1, n2) {
-//         return n1 + n2;
-//     },
-
-//     subtract: function(n1, n2) {
-//         return n1 - n2;
-//     },
-
-//     multiply: function(n1, n2) {
-//         return n1 * n2;
-//     },
-
-//     divide: function(n1, n2) {
-//         return n1 / n2;
-//     }
-// }
-
 const mathObj = {
     n1: 0,
     n2: 0,
@@ -48,14 +30,17 @@ const buttons = document.querySelectorAll("button");
 const display = document.getElementById("displayText");
 
 
-let operator;
+let operator = null;
 let lastBtnClicked;
 
 buttons.forEach(button => button.addEventListener("click", calculateNum));
 
-// Add a +/- button to give a negative or positive sign to a value
-
 function calculateNum(e) {
+    if(e.target.id === "clear") {
+        display.textContent = 0;
+        operator = null;
+        mathObj.n1 = 0;
+    }
     
     if (lastBtnClicked === "operator") {
         display.textContent = "";
@@ -77,72 +62,33 @@ function calculateNum(e) {
         }
     }
 
-
     if (e.target.className === "num") {
+        if(display.textContent === "0") {
+            display.textContent = "";
+        }
         display.textContent += e.target.textContent;
-    } else if (e.target.className === "operator" && display.textContent) {
-        if (!mathObj.n1) {
+    } else if (e.target.className === "operator" && display.textContent !== "0") {
+        if(!mathObj.n1 && lastBtnClicked === "equal") {
+            mathObj.n1 = Number(display.textContent);
+            operator = e.target.id;
+        } else if (!mathObj.n1 && operator === null) {
             mathObj.n1 = Number(display.textContent);
             operator = e.target.id;
         } else if (!mathObj.n2) {
             mathObj.n2 = Number(display.textContent);
             result = mathObj.operate(operator, mathObj.n1, mathObj.n2);
-            display.textContent = result;
+            display.textContent = result % 1 !== 0 ? parseFloat(result.toFixed(10)): result;
             operator = e.target.id;
             mathObj.n1 = result;
             mathObj.n2 = 0;
         }        
-    } else if (e.target.id === "equals" && mathObj.n1) {
+    } else if (e.target.id === "equal" && mathObj.n1) {
         mathObj.n2 = Number(display.textContent);
         result = mathObj.operate(operator, mathObj.n1, mathObj.n2);
-        display.textContent = result;
+        display.textContent = result % 1 !== 0 ? parseFloat(result.toFixed(10)): result;
         mathObj.n1 = 0;
         mathObj.n2 = 0;
     }
     
     lastBtnClicked = e.target.className;
-
 }
-
-
-// let num1 = mathObj.n1;
-// console.log(num1);
-// num1 = 2;
-// console.log(num1);
-
-// console.log(mathObj.n2);
-
-/*
-function calculateNum(e) {
-    let currentText = display.textContent;
-
-    
-    if (lastBtnClicked === "operator") {
-        display.textContent = "";
-    }
-
-    if (e.target.className === "num") {
-        displayText(e);
-    } else if (e.target.className === "operator" && currentText) {
-        if (!numOne) {
-            numOne = Number(display.textContent);
-            operator = e.target.id;
-        } else if (!numTwo) {
-            numTwo = Number(display.textContent);
-            result = operate(operator, numOne, numTwo);
-            display.textContent = result;
-            operator = e.target.id;
-            numOne = result;
-            numTwo = 0;
-        }        
-    } else if (e.target.id === "equals" && numOne) {
-        numTwo = Number(display.textContent);
-        result = operate(operator, numOne, numTwo);
-        display.textContent = result;
-        numOne = 0;
-        numTwo = 0;
-    }
-    
-    lastBtnClicked = e.target.className;
-}
-*/
